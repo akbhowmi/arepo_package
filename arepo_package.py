c7096dab784824933e3083592349f449d9269a33
Aklant Bhowmick <aklantbhowmick@login4.ufhpc>
2019-09-04 20:30:59 -0400

    remove 'sys' lines

--- arepo_package.py
+++ arepo_package.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append('/home/aklantbhowmick/anaconda3/lib/python3.7/site-packages')
+#sys.path.append('/home/aklantbhowmick/anaconda3/lib/python3.7/site-packages')
 
-sys.path.append('/home/aklantbhowmick/anaconda3/lib/python3.7/site-packages/scalpy/')
-sys.path.append('/home/aklantbhowmick/anaconda3/envs/nbodykit-env/lib/python3.6/site-packages/')
+#sys.path.append('/home/aklantbhowmick/anaconda3/lib/python3.7/site-packages/scalpy/')
+#sys.path.append('/home/aklantbhowmick/anaconda3/envs/nbodykit-env/lib/python3.6/site-packages/')
 
 #%pylab inline
 import h5py
